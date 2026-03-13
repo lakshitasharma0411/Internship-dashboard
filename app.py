3d84cbe2525da093588d2eb2237d3c15446ce8e8
lakshitasharma0411 <lakshitaaasharmaaa0987@gmail.com>
2026-03-13 20:22:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import pytz
+from datetime import datetime
 
 st.title("Global Job Preference Dashboard")
 
@@ -241,5 +243,6 @@ else:
 # Local URL: http://localhost:8501
 
 #  Network URL: http://192.168.0.21:8501
+
 
 
