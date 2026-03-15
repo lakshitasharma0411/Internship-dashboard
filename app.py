42e0746cb978cb28ddce0ec2ac1fb215b34ea657
lakshitasharma0411 <lakshitaaasharmaaa0987@gmail.com>
2026-03-15 12:55:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,8 @@ df["Job Posting Date"] = pd.to_datetime(df["Job Posting Date"], errors="coerce")
 # ---------------------------------------------------
 # TIME
 # ---------------------------------------------------
-
+ist = pytz.timezone("Asia/Kolkata")
+current_time = datetime.now(ist)
 
 
 # ===================================================
